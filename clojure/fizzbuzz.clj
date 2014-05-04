f56317928d78caf3113fdc5e59bc042955e514a0
Michael Gilliland <mjg.py3@gmail.com>
2014-05-04 10:28:52 -0400

    Tried clojure :)

---
+++ clojure/fizzbuzz.clj
@@ -0,0 +1,18 @@
+(use '[clojure.string :only (join)])
+
+(defn div-by [a b] (= (mod a b) 0))
+
+(defn calc-fizzbuzz [n]
+  (if (and (div-by n 3) (div-by n 5))
+    "FizzBuzz"
+    (if (div-by n 3)
+      "Fizz"
+      (if (div-by n 5)
+        "Buzz"
+        (str n)
+      )
+    )
+  )
+)
+
+(println (join "\n" (map calc-fizzbuzz (range 1 101))))
