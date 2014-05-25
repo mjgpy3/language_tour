5cbc4ee2aca1facda4096f12cf47e37f701ffaeb
Michael J. Gilliland <mjg.py3@gmail.com>
2014-05-24 21:26:20 -0400

    Went crazy with the ruby example

--- ruby/fizz_buzz.rb
+++ ruby/fizz_buzz.rb
@@ -1,7 +1,45 @@
 #!/usr/bin/env ruby
 
+class Fixnum
+  def divisible_by?(other)
+    self % other == 0
+  end
+
+  def to_fizzbuzz
+    [three_value, five_value].join('')
+  end
+
+  private
+
+  def three_value
+    div_by_returns(3, 'Fizz')
+  end
+
+  def five_value
+    div_by_returns(5, 'Buzz')
+  end
+
+  def div_by_returns(num, value)
+    divisible_by?(num) ? value : ''
+  end
+end
+
+class String
+  def if_its_not_empty_otherwise(other)
+    empty? ? other : self
+  end
+end
+
+class String
+  def if_its_not_empty_otherwise(other)
+    empty? ? other : self
+  end
+end
+
+def fizzbuzz_of(num)
+  num.to_fizzbuzz.if_its_not_empty_otherwise(num)
+end
+
 (1..100).each do |x|
-  phrase = (x%3 == 0 ? 'Fizz' : '')
-  phrase += 'Buzz' if x%5 == 0
-  puts (phrase.empty? ? x : phrase)
+  puts fizzbuzz_of(x)
 end
