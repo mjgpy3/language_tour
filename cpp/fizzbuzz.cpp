c297c6ea2f92cb04a5f010dae9a9c737d31e22ed
Gilli (Michael Gilliland) <gillilandmj@upmc.edu>
2014-06-16 12:18:22 -0400

    Improved c++

--- cpp/fizzbuzz.cpp
+++ cpp/fizzbuzz.cpp
@@ -5,19 +5,15 @@ int main()
 {
 	for (int i = 0; i < 101; i += 1)
 	{
-		if (i%3 == 0 && i%5 == 0)
-		{
-			cout << "FizzBuzz";
-		}
-		else if (i%3 == 0)
+		if (i%3 == 0)
 		{
 			cout << "Fizz";
 		}
-		else if (i%5 == 0)
+		if (i%5 == 0)
 		{
 			cout << "Buzz";
 		}
-		else
+		if (i%5 != 0 && i%3 != 0)
 		{
 			cout << i;
 		}
