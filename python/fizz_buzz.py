e09f7052cec4ab47f6edb11183317c096e5698f5
Gilli (Michael Gilliland) <gillilandmj@upmc.edu>
2014-06-16 11:19:17 -0400

    One-lined python fizzbuzz

--- python/fizz_buzz.py
+++ python/fizz_buzz.py
@@ -1,9 +1,3 @@
 #!/usr/bin/env python
 
-for i in xrange(1, 101):
-    print_me = ''
-    if not i%3: print_me = 'Fizz'
-    if not i%5: print_me += 'Buzz'
-    if not print_me: print_me = i
-
-    print(print_me)
+print '\n'.join((not i%3)*'Fizz' + (not i%5)*'Buzz' or str(i) for i in xrange(1, 101))
