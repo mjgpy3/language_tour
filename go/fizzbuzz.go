79f1c7cb256a6ea0ee08670b494ab9a880266a92
Gilli (Michael Gilliland) <gillilandmj@upmc.edu>
2014-07-02 16:27:49 -0400

    Had some fun with go

--- go/fizzbuzz.go
+++ go/fizzbuzz.go
@@ -1,19 +1,40 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "strconv"
+)
+
+func divisibleBy(num int, den int) bool {
+    return num % den == 0
+}
+
+type Rule struct {
+    Divisor int
+    WackyPhrase string
+}
+
+func fizzBuzz(start int, end int, rules []Rule) {
+    for i := start; i <= end; i += 1 {
+        printMe := ""
+
+        for _, rule := range rules {
+            if divisibleBy(i, rule.Divisor) {
+                printMe += rule.WackyPhrase
+            }
+        }
+
+	if printMe == "" {
+            printMe = strconv.Itoa(i)
+        }
+        fmt.Println(printMe)
+    }
+}
 
 func main() {
-    for i := 1; i < 101; i += 1 {
+    rules := []Rule{
+        Rule{ Divisor: 3, WackyPhrase: "Fizz" },
+        Rule{ Divisor: 5, WackyPhrase: "Buzz" }}
 
-	if i%3 == 0 && i%5 == 0 {
-		fmt.Println("FizzBuzz")
-	} else if i%3 == 0 {
-		fmt.Println("Fizz")
-	} else if i%5 == 0 {
-		fmt.Println("Buzz")
-	} else {
-		fmt.Println(i);
-	}
-
-    }
+    fizzBuzz(1, 100, rules)
 }
